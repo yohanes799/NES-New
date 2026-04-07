1cc48b92cd2c1b5c856d1de417523c85854a8ced
yohanes799 <bahuryohanes08@gmail.com>
2026-04-07 22:22:29 +0700

    Change repository name to NES-New

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/News/',
+  base: '/NES-New/',
   plugins: [react()],
   server: {
     proxy: {
